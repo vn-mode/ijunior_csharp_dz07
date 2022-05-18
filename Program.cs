05fdd2386c6767fe2d30f095ae51fb0c4a86c5d1
VITALII NAGIBIN <nagibin87@yandex.ru>
2022-05-18 21:41:41 +0300

    dz07
    dz07

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace vn_mode_csharp_dz07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countPeople;
+            int waitingTime = 10;
+            int allTime;
+            int countHours;
+            int countMinutes;
+
+            Console.Write("Введите количество людей в очереди: ");
+            countPeople = Convert.ToInt32(Console.ReadLine());
+
+            allTime = countPeople * waitingTime;
+            countHours = allTime / 60;
+            countMinutes = allTime % 60;
+
+            Console.WriteLine($"Вам придётся простоять в очереди {countHours} часов и {countMinutes} минут.");
+        }
+    }
+}
