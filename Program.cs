1b76a3c5d5b71da59325126d9a03254df6cc5e2a
VITALII NAGIBIN <nagibin87@yandex.ru>
2022-05-18 22:40:17 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -9,17 +9,18 @@ namespace vn_mode_csharp_dz07
             int countPeople;
             int waitingTime = 10;
             int allTime;
-            int countHours;
-            int countMinutes;
+            int waitingHours;
+            int waitingMinutes;
+            int oneHour = 60;
 
             Console.Write("Введите количество людей в очереди: ");
             countPeople = Convert.ToInt32(Console.ReadLine());
 
             allTime = countPeople * waitingTime;
-            countHours = allTime / 60;
-            countMinutes = allTime % 60;
+            waitingHours = allTime / oneHour;
+            waitingMinutes = allTime % oneHour;
 
-            Console.WriteLine($"Вам придётся простоять в очереди {countHours} часов и {countMinutes} минут.");
+            Console.WriteLine($"Вам придётся простоять в очереди {waitingHours} часов и {waitingMinutes} минут.");
         }
     }
 }
